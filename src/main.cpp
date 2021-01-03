fc113d3d6aa4a7077867669d09e0f7a2acb17f6b
Dominic Marcello <dmarce504@gmail.com>
2021-01-03 08:27:18 -0600

    Added function to find determinant

--- src/main.cpp
+++ src/main.cpp
@@ -44,20 +44,19 @@ const double Yp = (1 - hefrac / 2.0);
 double Hubble(double a) {
 	return H0 * h * std::sqrt(omega_r / (a * a * a * a) + omega_m / (a * a * a) + omega_lambda);
 }
+#define LMAX 5
 
-#define NF 9
-
+#define Ni 0
+#define Thetai LMAX
+#define ThetaPi 2*LMAX
+#define deltaci (3*LMAX)
+#define ubi (3*LMAX+1)
+#define deltabi (3*LMAX+2)
+#define uci (3*LMAX+3)
+#define Phii (3*LMAX+5)
+#define Psii (3*LMAX+6)
+#define NF (3*LMAX+7)
 using state = std::array<double,NF>;
-
-#define Theta0i 0
-#define Theta1i 1
-#define deltaci 2
-#define ubi 3
-#define deltabi 4
-#define uci 5
-#define Phii 6
-#define N0i 7
-#define N1i 8
 
 double compute_Nele(double eta, double beta);
 double compute_Npos(double eta, double beta);
@@ -144,83 +143,7 @@ void compute_chemistry(double rho, double T, double &H, double &Hp, double &He, 
 	Hep = B0 * ne * He0 / (B0 * C0 + B0 * ne + ne * ne);
 	Hepp = B0 * C0 * He0 / (B0 * C0 + B0 * ne + ne * ne);
 	He = He0 - Hep - Hepp;
-//	printf("p&e end\n");
 }
-//
-//void cooling_rate(double H, double Hp, double He, double Hep, double Hepp, double z, double T, double &C1, double &C2, double &C3, double &C4, double &C5,
-//		double &C6, double &C7, double &C8, double &C9, double &C10, double &C11, double &C12, double &C13) {
-//	const auto ne = Hp + Hep + 2 * Hepp;
-//	const auto T3 = T / 1e3;
-//	const auto T5 = T / 1e5;
-//	const auto T6 = T / 1e6;
-//	const auto tev = T * 8.61732814974056E-05;
-//	const auto logtev = std::log(T);
-//	const auto tiny = std::numeric_limits<double>::min();
-//
-//	const auto k1 = std::exp(
-//			-32.71396786375 + 13.53655609057 * logtev - 5.739328757388 * std::pow(logtev, 2) + 1.563154982022 * std::pow(logtev, 3)
-//					- 0.2877056004391 * std::pow(logtev, 4) + 0.03482559773736999 * std::pow(logtev, 5) - 0.00263197617559 * std::pow(logtev, 6)
-//					+ 0.0001119543953861 * std::pow(logtev, 7) - 2.039149852002e-6 * std::pow(logtev, 8));
-//
-//	const auto k3 = std::exp(
-//			-44.09864886561001 + 23.91596563469 * logtev - 10.75323019821 * std::pow(logtev, 2) + 3.058038757198 * std::pow(logtev, 3)
-//					- 0.5685118909884001 * std::pow(logtev, 4) + 0.06795391233790001 * std::pow(logtev, 5) - 0.005009056101857001 * std::pow(logtev, 6)
-//					+ 0.0002067236157507 * std::pow(logtev, 7) - 3.649161410833e-6 * std::pow(logtev, 8));
-//
-//	const auto k5 = std::exp(
-//			-68.71040990212001 + 43.93347632635 * logtev - 18.48066993568 * std::pow(logtev, 2) + 4.701626486759002 * std::pow(logtev, 3)
-//					- 0.7692466334492 * std::pow(logtev, 4) + 0.08113042097303 * std::pow(logtev, 5) - 0.005324020628287001 * std::pow(logtev, 6)
-//					+ 0.0001975705312221 * std::pow(logtev, 7) - 3.165581065665e-6 * std::pow(logtev, 8));
-//
-//	if (T > 250) {
-//		C1 = 7.50e-19 * std::exp(-118348 / T) / (1.0 + std::sqrt(T / 100000));
-//	} else {
-//		C1 = 0.0;
-//	}
-//	if (T > 25) {
-//		C2 = 9.10e-27 * std::pow(T, -.1687) * std::exp(-13179 / T) / (1.0 + std::sqrt(T / 100000));
-//	} else {
-//		C2 = 0.0;
-//	}
-//	if (T > 1000) {
-//		C3 = 5.54e-17 * std::pow(T, -.397) * std::exp(-473638 / T) / (1.0 + std::sqrt(T / 100000));
-//	} else {
-//		C3 = 0.0;
-//	}
-//	C4 = 2.18e-11 * k1;
-//	C5 = 3.94e-11 * k3;
-//	C6 = 8.72e-11 * k5;
-//	if (T > 100) {
-//		C7 = 5.01e-27 * std::pow(T, -.1687) * std::exp(-55338 / T) / (1.0 + std::sqrt(T / 100000));
-//	} else {
-//		C7 = 0.0;
-//	}
-//	C8 = 8.70e-27 * std::sqrt(T) * std::pow(T3, -0.2) / (1.0 + std::pow(T6, 0.7));
-//	C9 = 1.55e-26 * std::pow(T, 0.3647);
-//	if (T > 1000) {
-//		C10 = 1.24e-13 * std::pow(T, -1.5) * (1 + 0.3 * std::exp(-94000 / T)) * std::exp(-470000 / T);
-//	} else {
-//		C10 = 0.0;
-//	}
-//	C11 = 3.48e-26 * std::sqrt(T) * std::pow(T / 1000, -0.2) / (1.0 + std::pow(T / 1000000, 0.7));
-//	C12 = 1.43e-27 * std::sqrt(T) * (1.1 + 0.34 * std::exp(-std::pow(5.50 - std::log10(T), 2) / 3.0));
-//	C13 = 5.64e-36 * std::pow(1 + z, 4) * (T);
-//
-//	C1 *= ne * H;
-//	C2 *= ne * ne * He;
-//	C3 *= ne * Hep;
-//	C4 *= ne * H;
-//	C5 *= ne * He;
-//	C6 *= ne * Hep;
-//	C7 *= ne * ne * Hep;
-//	C8 *= ne * Hp;
-//	C9 *= ne * Hep;
-//	C10 *= ne * Hep;
-//	C11 *= ne * Hepp;
-//	C12 *= ne * (Hp + Hep + Hepp);
-//	C13 *= ne;
-//
-//}
 
 double fermi_dirac(double k, double eta, double beta) {
 	if (eta < -5) {
@@ -364,12 +287,8 @@ void chemistry_update(double &H, double &Hp, double &He, double &Hep, double &He
 	double nemin = 0.0;
 	double nemid;
 	double err = 0.0;
-	//printf("%e %e \n", (K1) / (K1 + K2), T);
-//	K1 = 0.0;
 	const auto compute_ne = [&](double ne) {
 		using namespace constants;
-		//	printf( "%e\n", C);
-		//	printf( "%e\n",std::exp(3.0 * 13.6 * evtoerg / 4.0 / kb / T));
 		H = (H0 + dt * (H0 + Hp0) * K2 * ne) / (1 + dt * (J20 + (K1 + K2) * ne));
 		Hp = H0 + Hp0 - H;
 		const auto den = (-1 + Power(dt, 3) * J22 * (J21 + K3 * ne) * (J22 + (K5 - K6) * ne) - dt * (J21 + (K3 + K4 + K5 + K6) * ne)
@@ -400,7 +319,6 @@ void chemistry_update(double &H, double &Hp, double &He, double &Hep, double &He
 		err = 1.0 - nemin / nemax;
 	} while (err > 1.0e-10 && nemid > 1e-100);
 	ne = nemid;
-//	printf( "%e\n", ne);
 	using namespace constants;
 	constexpr auto evtoerg = 1.6021772e-12;
 	constexpr auto eps_1_H = 13.59844 * evtoerg;
@@ -504,49 +422,11 @@ void zero_order_universe(double amin, double amax, std::function<double(double)>
 	thomsonfunc = build_interpolation_function(thomson, amin, amax);
 }
 
-//
-auto build_electron_fraction_interpolation_function(double amin, double amax) {
-//	double minloga = std::log(amin);
-//	double maxloga = std::log(amax);
-//	double dloga = 1.0 / 256.0;
-//	int N = (maxloga - minloga) / dloga + 1;
-//	dloga = (maxloga - minloga) / N;
-//	N += 2;
-////	printf( "electron fraction table size = %i\n", N);
-//	maxloga += dloga;
-//	minloga -= dloga;
-//	dloga = (maxloga - minloga) / N;
-//	std::vector<double> values(N + 1);
-//	for (int i = 0; i <= N; i++) {
-//		double loga = i * dloga + minloga;
-//		double a = std::exp(loga);
-//		values[i] = compute_electron_fraction(rho_b(a), 2.73 * Theta / a);
-//	}
-//
-//	const auto func = [=](double loga) {
-//		if (loga - dloga < minloga || loga + dloga > maxloga) {
-//			printf("Error in build_electron_fraction_interpolation_function\n");
-//			abort();
-//		}
-//		int i1 = std::min(std::max(1, int((loga - minloga) / (dloga))), N - 2);
-//		int i0 = i1 - 1;
-//		int i2 = i1 + 1;
-//		int i3 = i2 + 1;
-//		const double c0 = values[i1];
-//		const double c1 = -values[i0] / 3.0 - 0.5 * values[i1] + values[i2] - values[i3] / 6.0;
-//		const double c2 = 0.5 * values[i0] - values[i1] + 0.5 * values[i2];
-//		const double c3 = -values[i0] / 6.0 + 0.5 * values[i1] - 0.5 * values[i2] + values[i3] / 6.0;
-//		double x = (loga - i1 * dloga - minloga) / dloga;
-////		return values[i2] * x + values[i1] * (1 - x);
-//		return std::max(c0 + c1 * x + c2 * x * x + c3 * x * x * x, 0.0);
-//	};
-//	return func;
-}
-
 void advance(state &u, double k, double a0, double a1, std::function<double(double)> cs, std::function<double(double)> thomson) {
 	const double logamin = std::log(a0);
 	const double logamax = std::log(a1);
 	double loga = logamin;
+	double eta = 1.0 / (a0 * Hubble(a0));
 	while (loga < logamax) {
 		double a, H, Om, Or, eps, fb, fc, fgam, sigma, R, Onu, Ogam, fnu;
 		const auto compute_parameters = [&]() {
@@ -562,27 +442,45 @@ void advance(state &u, double k, double a0, double a1, std::function<double(doub
 			R = (3.0 * Om) / (4.0 * Or * fgam);
 		};
 		compute_parameters();
-		const auto lam_max = eps + 4.5 * Om + 12 * Or + eps * eps + 3.0;
+		const auto lam_max = eps + 4.5 * Om + 12 * Or + eps * eps + 3.0 + 12 * Or / (eps * eps);
+//		printf( "%e %e\n", lam_max - 12 * Or / (eps*eps), 12 * Or / (eps*eps));
 		const auto dloga = std::min(1.0 / lam_max, logamax - loga);
 		const auto mass = fc * u[deltaci] + fb * u[deltabi];
-		const auto rad = fnu * u[N0i] + fgam * u[Theta0i];
+		const auto rad = fnu * u[Ni] + fgam * u[Thetai];
 		const auto mmom = fc * u[uci] + fb * u[ubi];
-		const auto rmom = fnu * u[N1i] + fgam * u[Theta1i];
+		const auto rmom = fnu * u[Ni + 1] + fgam * u[Thetai + 1];
 		const auto Phi = 1.5 / (eps * eps) * (4 * Or * (rad + rmom / eps) + Om * (mass + mmom / eps));
-//		printf( "%e %e\n", u[Phii], Phi);
+		u[Psii] = -u[Phii] - 12 * Or * (fgam * u[Thetai + 2] + fnu * u[Ni + 2]) / (eps * eps);
+//		printf( "%e\n", -u[Psii]/ u[Phii]);
 		const auto compute_dudt_exp = [&](state u) {
 			state dudt;
 			const auto mass = fc * u[deltaci] + fb * u[deltabi];
-			const auto rad = fnu * u[N0i] + fgam * u[Theta0i];
-			dudt[Phii] = (0.5 * Om * mass + 2.0 * Or * rad - ((1.0 / 3.0) * eps * eps + 1.0) * u[Phii]);
-			dudt[Theta0i] = -eps * u[Theta1i] - dudt[Phii];
-			dudt[Theta1i] = eps / 3.0 * (u[Theta0i] - u[Phii]);
-			dudt[N0i] = -eps * u[N1i] - dudt[Phii];
-			dudt[N1i] = eps / 3.0 * (u[N0i] - u[Phii]);
+			const auto rad = fnu * u[Ni + 0] + fgam * (u[Thetai + 0]);
+
+			dudt[Phii] = 0.5 * Om * mass + 2.0 * Or * rad - ((1.0 / 3.0) * eps * eps * u[Phii]) + u[Psii];
+
+			dudt[Thetai + 0] = -eps * u[Thetai + 1] - dudt[Phii];
+			dudt[Thetai + 1] = eps / 3.0 * (u[Thetai + 0] - 2.0 * u[Thetai + 2] + u[Psii]);
+
+			dudt[Ni + 0] = -eps * u[Ni + 1] - dudt[Phii];
+			dudt[Ni + 1] = eps / 3.0 * (u[Ni] - 2.0 * u[Ni + 2] + u[Psii]);
+
+			dudt[ThetaPi + 0] = -eps * u[ThetaPi + 1];
+			dudt[ThetaPi + 1] = eps / 3.0 * (u[ThetaPi + 0] - 2.0 * u[ThetaPi + 2]);
+
+			for (int l = 2; l < LMAX - 1; l++) {
+				dudt[Thetai + l] = eps / (2 * l + 1) * (l * u[Thetai + l - 1] - (l + 1) * u[Thetai + l + 1]);
+				dudt[ThetaPi + l] = eps / (2 * l + 1) * (l * u[ThetaPi + l - 1] - (l + 1) * u[ThetaPi + l + 1]);
+				dudt[Ni + l] = eps / (2 * l + 1) * (l * u[Ni + l - 1] - (l + 1) * u[Ni + l + 1]);
+			}
+			int l = LMAX - 1;
+			dudt[Thetai + l] = eps * u[Thetai + l - 1] - (l + 1) / (eta * a * H) * u[Thetai + l];
+			dudt[ThetaPi + l] = eps * u[ThetaPi + l - 1] - (l + 1) / (eta * a * H) * u[ThetaPi + l];
+			dudt[Ni + l] = eps * u[Ni + l - 1] - (l + 1) / (eta * a * H) * u[Ni + l];
 			dudt[deltaci] = -eps * u[uci] - 3 * dudt[Phii];
-			dudt[uci] = -u[uci] - eps * u[Phii];
+			dudt[uci] = -u[uci] + eps * u[Psii];
 			dudt[deltabi] = -eps * u[ubi] - 3 * dudt[Phii];
-			dudt[ubi] = -u[ubi] - eps * u[Phii] + std::pow(cs(loga), 2) * u[deltabi];
+			dudt[ubi] = -u[ubi] + eps * u[Psii] + std::pow(cs(loga), 2) * u[deltabi];
 			return dudt;
 		};
 		state u0 = u;
@@ -592,10 +490,30 @@ void advance(state &u, double k, double a0, double a1, std::function<double(doub
 		}
 
 		sigma = thomson(loga);
-		u[Theta1i] = (dloga * dloga * (3 * dudt[Theta1i] + dudt[ubi] * R) * sigma + 3 * R * u0[Theta1i]
-				+ dloga * (3 * dudt[Theta1i] * R + 3 * sigma * u0[Theta1i] + R * sigma * u0[ubi])) / (3. * (R + dloga * sigma + dloga * fgam * R * sigma));
-		u[ubi] = (dloga * dudt[ubi] * (R + dloga * fgam * R * sigma) + 3 * dloga * fgam * sigma * (dloga * dudt[Theta1i] + u0[Theta1i])
-				+ R * (1 + dloga * fgam * sigma) * u0[ubi]) / (R + dloga * sigma + dloga * fgam * R * sigma);
+		auto Theta10 = u[Thetai + 1];
+		auto ub0 = u[ubi];
+		auto Theta20 = u[Thetai + 2];
+		auto ThetaP00 = u[ThetaPi + 0];
+		auto ThetaP20 = u[ThetaPi + 2];
+		auto &Theta1 = u[Thetai + 1];
+		auto &ub = u[ubi];
+		auto &Theta2 = u[Thetai + 2];
+		auto &ThetaP0 = u[ThetaPi + 0];
+		auto &ThetaP2 = u[ThetaPi + 2];
+
+		Theta1 = (9 * dloga * sigma * Theta10 + 3 * R * (Theta10 + dloga * sigma * ub0)) / (3 * R + dloga * (9 + R) * sigma);
+		ub = (3 * dloga * sigma * Theta10 + R * (3 + dloga * sigma) * ub0) / (3 * R + dloga * (9 + R) * sigma);
+
+#define Rule(a,b) a = b
+		Rule(Theta2, ((10 + 3 * dloga * sigma) * Theta20 + dloga * sigma * (ThetaP00 + ThetaP20)) / (2. * (1 + dloga * sigma) * (5 + dloga * sigma)));
+		Rule(ThetaP0, (10 * ThetaP00 + dloga * sigma * (5 * Theta20 + 7 * ThetaP00 + 5 * ThetaP20)) / (2. * (1 + dloga * sigma) * (5 + dloga * sigma)));
+		Rule(ThetaP2, (5 * ThetaP20 + dloga * sigma * (Theta20 + ThetaP00 + 2 * ThetaP20)) / ((1 + dloga * sigma) * (5 + dloga * sigma)));
+
+		for (int l = 3; l < LMAX; l++) {
+			u[Thetai + l] /= (1.0 + dloga * sigma);
+			u[ThetaPi + l] /= (1.0 + dloga * sigma);
+		}
+		eta += dloga / (a * H);
 		loga += dloga;
 	}
 
@@ -603,28 +521,52 @@ void advance(state &u, double k, double a0, double a1, std::function<double(doub
 
 void initial_conditions(state &u, double k, double a) {
 	const auto eps = k / (a * Hubble(a));
-	u[Theta0i] = 0.5 * u[Phii];
-	u[Theta1i] = -eps / 6.0 * u[Phii];
-	u[N0i] = u[Theta0i];
-	u[N1i] = u[Theta1i];
-	u[deltaci] = 3.0 * u[Theta0i];
-	u[uci] = 3.0 * u[Theta1i];
-	u[deltabi] = 3.0 * u[Theta0i];
-	u[ubi] = 3.0 * u[Theta1i];
+	const auto Rnu = omega_nu / (0.75 * omega_m * a + omega_r);
+	u[Phii] = -(1.0 + 0.4 * Rnu) * u[Psii];
+	u[Thetai + 0] = -0.5 * u[Psii];
+	u[Thetai + 1] = eps / 6.0 * u[Psii];
+	u[Ni + 0] = u[Thetai + 0];
+	u[Ni + 1] = u[Thetai + 1];
+	u[Ni + 2] = u[Psii] / 30.0;
+	u[deltaci] = 3.0 * u[Thetai + 0];
+	u[uci] = 3.0 * u[Thetai + 1];
+	u[deltabi] = 3.0 * u[Thetai + 0];
+	u[ubi] = 3.0 * u[Thetai + 1];
+	for (int l = 0; l < LMAX; l++) {
+		u[ThetaPi + l] = 0.0;
+	}
+	for (int l = 2; l < LMAX; l++) {
+		u[Thetai + l] = 0.0;
+	}
+	for (int l = 3; l < LMAX; l++) {
+		u[Ni + l] = 0.0;
+	}
 }
 
 int main() {
-	double amin = 1e-8;
-	double amax = 1.0;
-	std::function<double(double)> cs, thomson;
-	zero_order_universe(amin, amax, cs, thomson);
-	for (double k = 1e-4; k <= 1; k *= 1.1) {
-		state u;
-		u[Phii] = 1.0;
-		initial_conditions(u, k, amin);
-		advance(u, k, amin, amax, cs, thomson);
-		printf("%e %e\n", k, k * std::pow(u[Phii], 2));
-	}
+	std::array<std::array<double, 3>, 3 > A = {{ { -1, -2, 3 }, { 4, 5, 6 }, { 7, 8, 0 }} };
+	printf( "%e\n", matrix_determinant<3>(A));
+
+	//	const auto f = [](cmplx x) {
+//		return (x + 1.0) * (x + 1.0) * x;
+//	};
+//	const auto dfdx = [](cmplx x) {
+//		return (x + 1.0) * (x + 1.0) + 2.0 * (x + 1.0) * x;
+//	};
+//	auto roots = find_roots(f, 3);
+//	printf("%e %e %e %e %e %e \n", roots[0].real(), roots[0].imag(), roots[1].real(), roots[1].imag(), roots[2].real(), roots[2].imag());
+
+//	double amin = 1e-8;
+//	double amax = 1.0;
+//	std::function<double(double)> cs, thomson;
+//	zero_order_universe(amin, amax, cs, thomson);
+//	for (double k = 1e-4; k <= 1; k *= 1.1) {
+//		state u;
+//		u[Psii] = 1.0;
+//		initial_conditions(u, k, amin);
+//		advance(u, k, amin, amax, cs, thomson);
+//		printf("%e %e\n", k, k * std::pow(u[Psii], 2));
+//	}
 //	using namespace constants;
 //	double Tgas = 1e6;
 //	double eps;
@@ -641,7 +583,7 @@ int main() {
 //	for( double a = 1e-8; a <= 1.0; a *= 1.01) {
 //		printf( "%e %e %e\n", 1/a-1, cs(std::log(a)), thomson(std::log(a)));
 //	}
-	//	for (double z = 0.001; z < 1.5; z += 0.1) {
+//	for (double z = 0.001; z < 1.5; z += 0.1) {
 //		printf("%e %e \n", z, hyper_geometric(0.5, 0.5, 1.5, z).real());
 //	}
 //	for (double x = 1; x < 4.0; x += 0.1) {
@@ -655,7 +597,7 @@ int main() {
 //		equation_of_state(rho_b(a), T, P, cs);
 //		printf("%e %e %e %e %e %e\n", a, rho_b(a), T, P, cs / clight, func(std::log(a)));
 //	}
-	//	auto func = build_electron_fraction_interpolation_function(1e-10, 1);
+//	auto func = build_electron_fraction_interpolation_function(1e-10, 1);
 //	double max_err = 0.0;
 //	int n = 0;
 //	for (double a = 1.0e-10; a <= 1.0; a *= 1.01) {
